2e0325d6b9378be2dc9568d3ce1023ec864e77cc
Rahul Bhuniya <rahulbhuniya7276@gmail.com>
2026-09-11 18:00:01 +0530

    for deploy

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ import chatRoutes from "./routes/chat.js";
 import authRoutes from "./routes/auth.js";
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
